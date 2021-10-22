e33da11238b9475978ccf285efb96a913b51886b
GINSENG-A7 <zhenya_bykov_1998@mail.ru>
2021-10-22 16:23:05 +0300

    Добавьте файлы проекта.

---
+++ AlternativeTriginimetry.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingLab2Framework
+{
+    class AlternativeTrigonometry
+    {
+        public static double Cos(double x)
+        {
+            double t = 1;
+            double sum = 1;
+            int p = 0;
+            while (Math.Abs(t / sum) > 0)
+            {
+                p++;
+                t = (-t * x * x) / ((2 * p - 1) * (2 * p));
+                sum += t;
+            }
+            return sum;
+        }
+        public static double Csc(double x)
+        {
+            return 1 / Math.Sin(x);
+        }
+        public static double Sec(double x)
+        {
+            return 1 / Cos(x);
+        }
+        public static double Ln(double x)
+        {
+
+            double count = 1;
+            double totalValue = 0;
+            double Iterations = 37;
+            double z = 1;
+            double powe = 1;
+            double y;
+
+            while (count <= Iterations)
+            {
+                for (int i = 0; i < powe; i++)
+                {
+                    z *= (x - 1) / (x + 1);
+                }
+                y = (1 / powe) * z;
+
+                totalValue = totalValue + y;
+                powe = powe + 2;
+                count++;
+                z = 1; //Without this line you will have very high powers
+            }
+
+            return 2 * totalValue;
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingLab2Framework
+{
+    class Tests
+    {
+        [TestFixture]
+        class ExpressionsTests
+        {
+            double FirstExpression_MyCos(double x)
+            {
+                if (x >= 0)
+                {
+                    return (((((Math.Tan(x) / Math.Tan(x)) + Math.Sin(x)) + Math.Tan(x)) * AlternativeTrigonometry.Csc(x)) * AlternativeTrigonometry.Sec(x));
+                }
+                return double.NaN;
+            }
+            double FirstExpression_CommonCos(double x)
+            {
+                if (x >= 0)
+                {
+                    return (((((Math.Tan(x) / Math.Tan(x)) + Math.Sin(x)) + Math.Tan(x)) * AlternativeTrigonometry.Csc(x)) * (1 / Math.Cos(x)));
+                }
+                return double.NaN;
+            }
+            double SecondExpression_MyLn(double x)
+            {
+                if (x > 0)
+                {
+                    return (((((Math.Log(x, 2) - Math.Log(x, 2)) * Math.Log10(x)) / (Math.Log(x, 3) / (Math.Log(x, 5) * Math.Log(x)))) + ((Math.Log(x) / (Math.Log10(x) / Math.Log(x, 2))) * Math.Log(x, 3))) * ((Math.Log(x, 2) * Math.Pow(Math.Log(x, 3), 3)) / ((Math.Log10(x) * Math.Log(x, 2)) * Math.Pow(Math.Log(x, 5), 2))));
+                }
+                return double.NaN;
+            }
+            double SecondExpression_CommonLn(double x)
+            {
+                if (x > 0)
+                {
+                    return (((((Math.Log(x, 2) - Math.Log(x, 2)) * Math.Log10(x)) / (Math.Log(x, 3) / (Math.Log(x, 5) * AlternativeTrigonometry.Ln(x)))) + ((AlternativeTrigonometry.Ln(x) / (Math.Log10(x) / Math.Log(x, 2))) * Math.Log(x, 3))) * ((Math.Log(x, 2) * Math.Pow(Math.Log(x, 3), 3)) / ((Math.Log10(x) * Math.Log(x, 2)) * Math.Pow(Math.Log(x, 5), 2))));
+                }
+                return double.NaN;
+            }
+
+            [Test]
+            public void ExpressionWithMyCosIsEqualToDefaul()
+            {
+                Assert.AreEqual(Math.Round(FirstExpression_MyCos(5), 12, MidpointRounding.AwayFromZero), Math.Round(FirstExpression_CommonCos(5), 12, MidpointRounding.AwayFromZero));
+            }
+            [Test]
+            public void ExpressionWithMyLnIsEqualToDefaul()
+            {
+                Assert.AreEqual(Math.Round(SecondExpression_MyLn(5), 12, MidpointRounding.AwayFromZero), Math.Round(SecondExpression_CommonLn(5), 12, MidpointRounding.AwayFromZero));
+            }
+        }
+    }
+}
